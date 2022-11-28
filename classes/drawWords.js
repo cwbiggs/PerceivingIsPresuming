3acb942cdb95a9ee2daa8b60b947ee5b05b9af7f
cwbiggs <33208217+cwbiggs@users.noreply.github.com>
2022-11-28 14:47:53 -0500

    Create drawWords.js

---
+++ classes/drawWords.js
@@ -0,0 +1,18 @@
+function drawWords() {
+  var currentWords;
+
+  if (correctAudio) {
+    currentWords = 1;
+  } else {
+    currentWords = 0;
+  }
+  
+  noStroke();
+  fill(0);
+  rect(60, 110, 300, 25);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(54, 255, 0, 255);
+  noStroke();
+  text(words[currentWords], 60, 110);
+}
